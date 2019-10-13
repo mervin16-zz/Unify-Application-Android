ed85370815a1306f977a26a8a929f84d1ddce1e8
Mervin Hemaraju <mervinhemaraju16@gmail.com>
2019-10-13 18:22:26 +0400

    Backdrop Added & Changed Launcher Icon
    - Added backdrop material design on main screen for navigation  - Added new palette and theme for application  - Changed default launcher icon of application

--- app/src/main/java/com/th3pl4gu3/unify/ActivityMain.kt
+++ app/src/main/java/com/th3pl4gu3/unify/ActivityMain.kt
@@ -2,7 +2,9 @@ package com.th3pl4gu3.unify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.Menu
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.th3pl4gu3.unify.databinding.ActivityMainBinding
 
@@ -15,8 +17,18 @@ class ActivityMain : AppCompatActivity() {
 
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        _binding.materialButton.setOnClickListener{
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
-        }
+        setSupportActionBar(_binding.ToolBarMainActivityMain)
+
+        _binding.ToolBarMainActivityMain.setNavigationOnClickListener(BackdropClickListener(
+            this,
+            _binding.ContainerMainContent,
+            AccelerateDecelerateInterpolator(),
+            ContextCompat.getDrawable(this, R.drawable.ic_menu_accent),
+            ContextCompat.getDrawable(this, R.drawable.ic_close_accent)))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
